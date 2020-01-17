91f4d1c6ceeb02b80976a5930e482c0723ecb3f4
Roman Ryzhikov <general@ya.ru>
2020-01-17 17:51:04 +0300

    Update elevator.cs

--- elevator.cs
+++ elevator.cs
@@ -1,6 +1,21 @@
+using Sandbox.Game.EntityComponents;
 using Sandbox.ModAPI.Ingame;
-using System;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
 using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
 
 namespace IngameScript
 {
@@ -22,7 +37,7 @@ namespace IngameScript
 
         public void Main(string argument, UpdateType updateSource)
         {
-
+              
             if (argument != "")
                 FloorHeight = Convert.ToSingle(argument);
 

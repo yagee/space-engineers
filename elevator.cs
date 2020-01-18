9ebc0ad73bc5aa4d7d76ae4bcdeba98cf4e00e7c
Roman Ryzhikov <general@ya.ru>
2020-01-18 12:41:26 +0300

    Update elevator.cs

--- elevator.cs
+++ elevator.cs
@@ -2,66 +2,63 @@ using Sandbox.Game.EntityComponents;
 using Sandbox.ModAPI.Ingame;
 using Sandbox.ModAPI.Interfaces;
 using SpaceEngineers.Game.ModAPI.Ingame;
-using System.Collections.Generic;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System;
+using VRage;
 using VRage.Collections;
+using VRage.Game;
 using VRage.Game.Components;
 using VRage.Game.GUI.TextPanel;
-using VRage.Game.ModAPI.Ingame.Utilities;
 using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
 using VRage.Game.ObjectBuilders.Definitions;
-using VRage.Game;
-using VRage;
 using VRageMath;
 
 namespace IngameScript
 {
-    partial class Program : MyGridProgram
+  partial class Program : MyGridProgram
+  {
+    List<IMyPistonBase> pistons;
+
+    IMyBlockGroup pistonGroup;
+
+    float floorHeight;
+
+    public Program()
     {
-        List<IMyPistonBase> Pistons;
-        IMyBlockGroup PistonGroup;
-
-        float FloorHeight;
-
-        public Program()
-        {
-            Pistons = new List<IMyPistonBase>();
-            PistonGroup = GridTerminalSystem.GetBlockGroupWithName("ElevatorPistons");
-            PistonGroup.GetBlocksOfType<IMyPistonBase>(Pistons);
-
-            Runtime.UpdateFrequency = UpdateFrequency.Update1;
-        }
-
-        public void Main(string argument, UpdateType updateSource)
-        {
-              
-            if (argument != "")
-                FloorHeight = Convert.ToSingle(argument);
-
-            Runtime.UpdateFrequency = UpdateFrequency.None;
-
-            foreach (IMyPistonBase Piston in Pistons)
-            {
-                float CurrentHeight = Piston.CurrentPosition;
-                float PistonVelocity = FloorHeight - CurrentHeight;
-                if (Math.Abs(PistonVelocity) > 0.01)
-                {
-                    Runtime.UpdateFrequency = UpdateFrequency.Update1;
-                }
-                else
-                {
-                    PistonVelocity = 0;
-                }
-                Piston.Velocity = ConstrainNum(PistonVelocity, -0.2f, 0.2f);
-            }
-        }
-
-        float ConstrainNum(float Num, float Min, float Max)
-        {
-            return Math.Max(Math.Min(Max, Min), Min);
-        }
+      pistons = new List<IMyPistonBase>();
+      pistonGroup = GridTerminalSystem.GetBlockGroupWithName("ElevatorPistons");
+      pistonGroup.GetBlocksOfType<IMyPistonBase>(pistons);
     }
+
+    public void Main(string argument)
+    {
+      if (argument != "")
+        floorHeight = Convert.ToSingle(argument);
+
+      Runtime.UpdateFrequency = UpdateFrequency.None;
+      foreach (IMyPistonBase piston in pistons)
+      {
+        float currentHeight = piston.CurrentPosition;
+        float pistonVelocity = floorHeight - currentHeight;
+        if (Math.Abs(pistonVelocity) > 0.05)
+        {
+          Runtime.UpdateFrequency = UpdateFrequency.Update1;
+        }
+        else
+        {
+          piston.Velocity = 0;
+        }
+        piston.Velocity = ConstrainNum(pistonVelocity, -1, 1);
+      }
+    }
+
+    float ConstrainNum(float Num, float Min, float Max)
+    {
+      return Math.Max(Math.Min(Max, Num), Min);
+    }
+  }
 }
